18ebeea4e28cf7347f81e29fe8ee385f978ead3b
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2022-10-08 23:00:28 +0530

    feat: switch to loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,29 +1,35 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
-    
+
     let system_guess = rand::thread_rng().gen_range(1..=100);
 
     println!("Please input your guess.");
 
-    let mut guess = String::new();
+    loop {
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    println!("The system guessed: {}", system_guess);
+        // println!("The system guessed: {}", system_guess);
 
-    match guess.cmp(&system_guess) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&system_guess) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                println!("The system guessed {} as well!", system_guess);
+                break;
+            }
+        }
     }
 }
